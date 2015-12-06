4414ef9b7d9b801867ad7b3f73c3d4107eaf7371
Ann Catton <anncatton@gmail.com>
2015-12-06 01:13:09 +0000

    icon doesn't show up where it's NOT ALLOWED.

--- extension_sample/soundbox_browser.js
+++ extension_sample/soundbox_browser.js
@@ -1,22 +1,17 @@
 chrome.runtime.onInstalled.addListener(function() {
-// Replace all rules ...
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-  // With a new rule ...
-  chrome.declarativeContent.onPageChanged.addRules([
-    {
-      conditions: [
-        new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: { hostEquals: 'www.soundcloud.com', schemes: ['https'] },
-        })
-      ],
-      // And shows the extension's page action.
-      actions: [ new chrome.declarativeContent.ShowPageAction() ]
-    }
-  ]);
-});
-});
-
-chrome.tabs.onUpdated.addListener(function(id, info, tab){
-  chrome.pageAction.show(tab.id);
-  chrome.tabs.executeScript(null, {"file": "soundbutton.js"});
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'soundcloud.com' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
 });
