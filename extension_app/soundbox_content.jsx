64c97ad5fa15f65b1be2bfc0a89761f8855740b0
Martin Laws <martinblaws@gmail.com>
2015-12-07 17:51:01 -0500

    added NPM

---
+++ extension_app/soundbox_content.jsx
@@ -0,0 +1,12 @@
+
+
+
+// $().ready(function() {
+//   var custom_icon = chrome.extension.getURL("box.png");
+//   var $soundBoxButton = $('<button class="icon-button"><img class="sc-button-small sc-button-responsive" title="Add to Soundbox" src="' + custom_icon + '" /></button>');
+//
+//   var $customIconImage = $('<img class="icon" title="Add to Soundbox" src="' + custom_icon + '" />');
+//     $('.soundActions.sc-button-toolbar.soundActions__small').append($soundBoxButton).on('click', function() {
+//       alert('test!')
+//     })
+// });
