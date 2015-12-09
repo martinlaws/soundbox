7fc98ad5f1462ca0024689f0b18cc6bda66cd70a
Ann Catton <anncatton@gmail.com>
2015-12-09 15:53:29 -0500

    removed display toggle for now because it haven't gotten it to work consistently.

--- extension_app/soundbox_content.js
+++ extension_app/soundbox_content.js
@@ -1,7 +1,7 @@
 $().ready(function() {
 
   var boxIcon = chrome.extension.getURL("box.png");
-  var $soundBoxButton = $('<button class="icon-button"><img class="icon" title="Add to Soundbox" src="' + boxIcon + '" /></button>'); 
+  var $soundBoxButton = $('<button class="icon-button hide"><img class="icon" title="Add to Soundbox" src="' + boxIcon + '" /></button>'); 
 
   function addButton() {
      $('.sound__artwork').not('.appended').addClass('appended').append($soundBoxButton);
@@ -9,14 +9,16 @@ $().ready(function() {
 
   setInterval(addButton, 1000);
 
-  $('.soundList__item').on('mouseover', function() {
-    $(this).find('.icon-button').removeClass('hide');
-    $(this).find('.icon-button').addClass('show');
-  });
+  // $('.soundList__item').on('mouseover', function() {
+  //   $(this).find('.icon-button').show();
+  //   $(this).find('.icon-button').removeClass('hide');
+  //   $(this).find('.icon-button').addClass('show');
+  // });
 
-  $('.soundList__item').on('mouseleave', function() {
-    $(this).find('.icon-button').removeClass('show');
-    $(this).find('.icon-button').addClass('hide');
-  });
+  // $('.soundList__item').on('mouseleave', function() {
+  //   $(this).find('.icon-button').hide();
+  //   $(this).find('.icon-button').removeClass('show');
+  //   $(this).find('.icon-button').addClass('hide');
+  // });
 
 });
