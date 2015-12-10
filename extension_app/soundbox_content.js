dc66f9d4fc1407a20b45a8b5ed5283275ceae0d1
Ann Catton <anncatton@gmail.com>
2015-12-10 12:44:33 -0500

    option menu can be toggled on and off

--- extension_app/soundbox_content.js
+++ extension_app/soundbox_content.js
@@ -1,9 +1,12 @@
 $().ready(function() {
 
   var boxIcon = chrome.extension.getURL("box.png");
+  var closeIcon = chrome.extension.getURL("close.png");
+
   var $soundBoxButton = $('<button class="icon-button"><img class="icon" title="Add to Soundbox" src="' + boxIcon + '" /></button>');
 
-  var $soundBoxMenu = $('<div id="option-menu"><button class="close">X</button></div>');
+  var $soundBoxMenu = $('<div id="option-menu"><button class="close"><img class="icon" id="close-button" title="Close Window" src="' + 
+    closeIcon + '" /></button></div>');
 
   $('body').append($soundBoxMenu);
 
@@ -18,6 +21,10 @@ $().ready(function() {
     var playerTrackURL = $('.playbackSoundBadge a').prop('href');
     return playerTrackURL; // so this where you'd have to store to a db because the timeout call only returns its timeout ID
   }
+
+  $('#close-button').on('click', function() {
+    $('#option-menu').removeClass('show-menu');
+  });
 
   $('.compactTrackListItem__content').on('click', function() {
     var playerUrl = setTimeout(findPlayedTrackURL, 2000);
