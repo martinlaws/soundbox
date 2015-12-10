5d52e6f1dab77ade5396890008f50936789acd5f
Ann Catton <anncatton@gmail.com>
2015-12-09 19:15:38 -0500

    click grabs link from track image

--- extension_app/soundbox_content.js
+++ extension_app/soundbox_content.js
@@ -1,24 +1,15 @@
 $().ready(function() {
 
   var boxIcon = chrome.extension.getURL("box.png");
-  var $soundBoxButton = $('<button class="icon-button hide"><img class="icon" title="Add to Soundbox" src="' + boxIcon + '" /></button>'); 
+  var $soundBoxButton = $('<button class="icon-button"><img class="icon" title="Add to Soundbox" src="' + boxIcon + '" /></button>'); 
 
   function addButton() {
-     $('.sound__artwork').not('.appended').addClass('appended').append($soundBoxButton);
+    $('.sound__artwork').not('.appended').addClass('appended').append($soundBoxButton).on('click', function() {
+      var trackURL = $(this).children('a').prop('href');
+      console.log(trackURL);
+    });
   }
 
   setInterval(addButton, 1000);
-
-  // $('.soundList__item').on('mouseover', function() {
-  //   $(this).find('.icon-button').show();
-  //   $(this).find('.icon-button').removeClass('hide');
-  //   $(this).find('.icon-button').addClass('show');
-  // });
-
-  // $('.soundList__item').on('mouseleave', function() {
-  //   $(this).find('.icon-button').hide();
-  //   $(this).find('.icon-button').removeClass('show');
-  //   $(this).find('.icon-button').addClass('hide');
-  // });
 
 });
