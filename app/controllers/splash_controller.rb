2fe5c3436a956cce64adabd8a07c6072fbb6f524
dorianwolf <dorian.wolf@gmail.com>
2016-03-23 01:40:24 -0400

    small styling to splash

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,6 +1,5 @@
 class SplashController < ApplicationController
   # before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :require_user
 
   def splash
     render :'splash/index', layout: false
