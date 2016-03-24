7e86661e9b57f42cd77ced9d8fd0297dd2b08480
dorianwolf <dorian.wolf@gmail.com>
2016-03-24 16:08:30 -0400

    fixing smtp auth for the mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'yeahisaidit.herokuapp.com',
+    domain:               'soundbox-app.herokuapp.com',
     user_name:            ENV["GMAIL_USERNAME"],
     password:             ENV["GMAIL_PASS"],
     authentication:       'plain',
