b6de8451f44f59273f23dd5bc54867d05e555a1b
dorianwolf <dorian.wolf@gmail.com>
2016-03-24 20:48:52 -0400

    actually emails to the email inputted

--- app/mailers/opt_in_mailer.rb
+++ app/mailers/opt_in_mailer.rb
@@ -4,7 +4,7 @@ class OptInMailer < ApplicationMailer
 
   def opt_in_email(subscriber)
     @subscriber = subscriber
-    mail(to: 'grant@soundbox.co', subject: 'new message')
+    mail(to: @subscriber.email, subject: 'new message')
   end
 
 end
